fd0eed32f1a40bd0368222f7b21d914926a7ff53
AleMag <alex.senyuta@mail.ru>
2024-04-28 22:17:50 +0300

    Попытка 2. Исправил PEP8

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -58,7 +58,8 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         verbose_name="Дата и время публикации",
         null=False,
-        help_text="Если установить дату и время в будущем — можно делать отложенные публикации."
+        help_text="Если установить дату и время в будущем — "
+                  "можно делать отложенные публикации."
     )
     author = models.ForeignKey(
         User,
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.http import Http404
 
-from .models import Post, Category, Location
+from .models import Post, Category
 
 
 def index(request):
